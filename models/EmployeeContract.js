154d87494bd2b979bd2144be70cd20890bac0f73
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-06-17 19:43:50 +0300

    Solve color

---
+++ models/EmployeeContract.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const employeeContractSchema = new mongoose.Schema(
+  {
+    employeeName: { type: String, required: true, trim: true },
+    nationalId: { type: String, required: true, trim: true },
+    birthDate: { type: String, required: true },
+    contractDuration: { type: Number, required: true },
+    startDate: { type: String, required: true },
+    endDate: { type: String, required: true },
+    contractDate: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("EmployeeContract", employeeContractSchema);
